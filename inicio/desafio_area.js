451fa460b554986a4b1e3e17378c754693ad04ab
Jamine Machado <jaminekelly02@gmail.com>
2024-08-09 21:56:27 -0300

    estudos completo

---
+++ inicio/desafio_area.js
@@ -0,0 +1,16 @@
+// PI = 3.14
+// PI * raio * raio
+
+const PI = 3.14;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("o valor da área é" + areaCirc + "m2.");
+
+areaCirc = Math.PI * raio * raio;
+console.log("o valor da área é" + areaCirc + "m2.");
+
+
+
+
+console.log(Math.PI);
---
+++ inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,32 @@
+/*
+    // Bloco 1
+    1
+    3
+    5
+    7
+    9
+
+    // Bloco 2
+    2
+    4
+    6
+    8
+    10
+
+*/
+
+{
+    console.log("Passo #01");
+    console.log("Passo #03");
+    console.log("Passo #05");
+    console.log("Passo #07");
+    console.log("Passo #09");
+}
+
+{
+    console.log("Passo #02");
+    console.log("Passo #04");
+    console.log("Passo #06");
+    console.log("Passo #08");
+    console.log("Passo #010");
+}
